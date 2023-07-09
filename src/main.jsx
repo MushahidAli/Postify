b6bb1813cb80ca747e3514294b1f0156e374e717
Mohammed Mushahid Ali <alimushahid24@gmail.com>
2023-07-09 15:48:33 +0530

    Postify Application :)

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
